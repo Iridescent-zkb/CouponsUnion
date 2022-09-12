bc12b6a4e39917fa88ef5a3704b0d1c4dcaab069
iridescemt <2468484105zhu@gmail.com>
2022-09-12 23:34:20 +0800

    实现主界面的切换

--- app/src/main/java/com/example/couponsunion/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/couponsunion/ui/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.couponsunion.ui.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -10,7 +11,11 @@ import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.couponsunion.R;
+import com.example.couponsunion.base.BaseFragment;
 import com.example.couponsunion.ui.fragment.HomeFragment;
+import com.example.couponsunion.ui.fragment.RedPacketFragment;
+import com.example.couponsunion.ui.fragment.SearchFragment;
+import com.example.couponsunion.ui.fragment.SelectedFragment;
 import com.example.couponsunion.utils.LogUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -24,13 +29,28 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     @BindView(R.id.main_navigation_bar)
     public BottomNavigationView  mNavigationView;
+    private HomeFragment mHomeFragment;
+    private RedPacketFragment mRedPacketFragment;
+    private SelectedFragment mSelectedFragment;
+    private SearchFragment mSearchFragment;
+    private FragmentManager mFm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        initView();
+        initFragments();
         initListener();
+    }
+
+    private void initFragments() {
+        mHomeFragment = new HomeFragment();
+        mRedPacketFragment = new RedPacketFragment();
+        mSelectedFragment = new SelectedFragment();
+        mSearchFragment = new SearchFragment();
+        mFm = getSupportFragmentManager();
+        switchFragment(mHomeFragment);
     }
 
     private void initListener() {
@@ -38,23 +58,26 @@ public class MainActivity extends AppCompatActivity {
 //                Log.d(TAG,"title -->" + item.getTitle() + "id --> " + item.getItemId());
             if (item.getItemId() == R.id.home){
                 LogUtils.d(MainActivity.class,"切换到首页");
+                switchFragment(mHomeFragment);
             }else if (item.getItemId() == R.id.selected){
                 LogUtils.i(MainActivity.class,"切换到精选");
+                switchFragment(mRedPacketFragment);
             }else if (item.getItemId() == R.id.red_packet){
                 LogUtils.w(MainActivity.class,"切换到特惠");
+                switchFragment(mSelectedFragment);
             }else if (item.getItemId() == R.id.search){
                 LogUtils.e(MainActivity.class,"切换到搜索");
+                switchFragment(mSearchFragment);
             }
             return true;
         });
     }
 
-    private void initView() {
-
-        HomeFragment homeFragment = new HomeFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.add(R.id.main_page_container,homeFragment);
-        transaction.commit();
+    private void switchFragment(BaseFragment targetFragment) {
+        FragmentTransaction fragmentTransaction = mFm.beginTransaction();
+        fragmentTransaction.replace(R.id.main_page_container,targetFragment);
+        fragmentTransaction.commit();
     }
+
+
 }
